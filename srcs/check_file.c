32b935931ec04246e5f77717d18c84eff94ecc32
Florian Lombardo <florianlombardo3@gmail.com>
2019-07-02 13:38:04 +0200

    Update check_file.c

--- srcs/check_file.c
+++ srcs/check_file.c
@@ -13,7 +13,52 @@
 
 #include "../includes/parsing.h"
 
-int		check_file(char *filename)
+static int	check_chars(char *str)
 {
-	;
+	int		i;
+
+	i = -1;
+	while (str[++i])
+		if ((str[i] < '0' || str[i] > '9') && str[i] != ' ')
+			return (0);
+	return (1);
+}
+
+static int	check(int fd)
+{
+	int		len;
+	char	*line;
+	
+	if (!(get_next_line(fd, &line)))
+	{
+		ft_strdel(&line);
+		return (1);
+	}
+	len = ft_strlen(line);
+	ft_strdel(&line);
+	while (get_next_line(fd, &line))
+	{
+		if (len != ft_strlen(line) || !check_chars(line))
+		{
+			ft_strdel(&line);
+			return (1);
+		}
+		ft_strdel(&line);
+	}
+	return (0);
+}
+
+int			check_file(char *filename)
+{
+	int		fd;
+	int		ret;
+	
+	ret = 0;
+	if ((fd = open(filename, O_RDONLY)) == -1)
+		return (1);
+	if (!check(fd))
+		ret = 1;
+	if (close(fd) == -1)
+		return (1);
+	return (ret);
 }
